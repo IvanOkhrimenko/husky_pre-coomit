201519fef71b56afccf13d4f23ecc99071784f80
Ivam Okhrimenko <okhrim_i@Ivams-MacBook-Pro.local>
2020-09-01 21:12:36 +0300

    second commit, render different title for server and browser

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { isPlatformServer } from '@angular/common';
+import { Component, Inject, PLATFORM_ID } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'testProject';
+  title = 'клиент';
+
+  constructor(@Inject(PLATFORM_ID) platformId) {
+    if (isPlatformServer(platformId)) {
+      this.title = 'SSR';
+    }
+  }
 }
